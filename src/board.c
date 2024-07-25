824764d0efb3850784aafb150c01c57436eb5cfd
Ruslan Bukin <br@freebsd.org>
2024-07-25 20:26:13 +0100

    drop "All rights reserved" line.

--- src/board.c
+++ src/board.c
@@ -1,6 +1,5 @@
 /*-
  * Copyright (c) 2024 Ruslan Bukin <br@bsdpad.com>
- * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
